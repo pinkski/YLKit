70fbe64cd01fb90a2eeb454e0bd7069f3d622c14
lynn.zhu <zxl20zxl@gmail.com>
2016-07-27 16:48:50 +0800

    first commit

---
+++ YLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLKit
+//
+//  Created by 张晓岚 on 16/7/27.
+//  Copyright © 2016年 ThinkMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
